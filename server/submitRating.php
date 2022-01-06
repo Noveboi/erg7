e368ed3f947abf29ca537cc038774279e1772e1a
Noveboi <cptgiorgos03@gmail.com>
2022-01-06 16:36:22 +0200

    Added POST information

--- server/submitRating.php
+++ server/submitRating.php
@@ -7,4 +7,26 @@ $password = "";
 $dbname = "geniki_erg";
 
 $conn = new mysqli($hostname, $username, $password, $dbname);
+
+if($conn->connect_error)
+{
+	die("Connection failed: " . $conn->connect_error);
+}
+echo "Connection successful";
+
+//Get POSTed form data
+//(Βαλε τα corresponding names στα form inputs)
+$discName = $_POST['disc'];
+$reldate = $_POST['reldate'];
+$uname = $_POST['username'];
+$reviewtext = $_POST['review'];
+$rating = $_POST['rating'];
+
+//DEBUG: Print POST values
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
+
+// header("Location: contest.html");
+// exit();
 ?>
