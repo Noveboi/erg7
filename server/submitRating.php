90fddf4bb95067b83c5cca9b6ef663bbbf99b842
Noveboi <cptgiorgos03@gmail.com>
2022-01-06 16:17:31 +0200

    Added submitRating.php and setup connection

---
+++ server/submitRating.php
@@ -0,0 +1,10 @@
+<?php 
+//Establish connection to SQL Server and submit form data
+
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$dbname = "geniki_erg";
+
+$conn = new mysqli($hostname, $username, $password, $dbname);
+?>
