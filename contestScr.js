c539b3bd22ffdbcc61bcdec2f31fab7bc0d34f9c
Noveboi <cptgiorgos03@gmail.com>
2022-01-09 13:58:25 +0200

    PHP to JS data transmit complete, contestScr.js 90% complete

--- contestScr.js
+++ contestScr.js
@@ -11,15 +11,56 @@ xhr.open("get", "getData.php", true);
 xhr.send();
 
 var data = (sessionStorage.getItem('phpdata') != null) ? sessionStorage.getItem('phpdata') : "";
-console.log(typeof data);
 
-console.log(data);
+//String formatting...
+var it = 0;
 
 data = data.replace(data[0],"");
 while(data.includes('\"\"')) {
     data = data.replace('\"\"', ",");
+    it++;
 }
 data = data.replace('"', "");
 
+var datasub = data;
+var commaindex = 0;
 console.log(data);
-console.log("hello");
+var iter = 0;
+
+var userArray = [];
+var tempArray = [];
+
+for(let i = 0; i <= it; i++) {
+    
+    var nextsubstart = (i != 0) ? data.indexOf(",", commaindex) + 1: 0;
+    if(i != it) {
+        commaindex = data.indexOf(",", nextsubstart);
+    } else {
+        tempArray.push(data.substring(commaindex + 1, data.length));
+        t = [tempArray[0], tempArray[1]];
+        userArray.push(t);
+    }
+    datasub = data.substring(nextsubstart, commaindex);
+    if(i % 2 == 0 && iter < 2) {
+        iter++;
+        tempArray.push(datasub); //Pushes username
+        console.log(tempArray, iter, i)
+    }
+    else if (i % 2 == 1 && iter < 2) {
+        iter++;
+        tempArray.push(datasub); //Pushes birthyear
+        console.log(tempArray, iter, i);
+    }
+    else {
+        iter = 0;
+        t = [tempArray[0], tempArray[1]];
+        userArray.push(t);
+        tempArray.pop();
+        tempArray.pop();
+        tempArray.push(datasub); //Pushes username
+        iter++;
+        console.log("reset");
+    }
+}
+
+//Data translation from PHP to JS over, time to 
--- getData.php
+++ getData.php
@@ -16,29 +16,14 @@ if($conn->connect_error)
 $getdata = "SELECT uname, birthyear FROM reviews";
 
 $result = $conn->query($getdata);
-$reviewamt = 0;
-$finalArray = array();
 
 if($result->num_rows > 0)
 {
 	while($row = $result->fetch_assoc()) {
-		$reviewamt++;
-		// array_push($finalArray, array($row['uname'], $row['birthyear']));
         echo json_encode($row['uname']);
         echo json_encode($row['birthyear']);
 	}
 } else {
     echo "no results";
 }
-
-// $q = $_REQUEST['q'];
-// if($q !== "") {
-// 	if($q == "sendthegoodstuff") {
-// 		echo json_encode($finalArray, JSON_OBJECT_AS_ARRAY);
-//         echo json_encode("test");
-//         echo json_encode("test2");
-// 	} else {
-// 		echo "yikes";
-// 	}
-// }
 ?>
