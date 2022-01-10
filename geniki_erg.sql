29d87c030c273f145f32119024e983e25f2d4c75
Noveboi <cptgiorgos03@gmail.com>
2022-01-10 21:27:39 +0200

    updated database

--- geniki_erg.sql
+++ geniki_erg.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 10, 2022 at 06:04 PM
+-- Generation Time: Jan 10, 2022 at 08:26 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.0
 
@@ -49,7 +49,12 @@ INSERT INTO `reviews` (`reviewid`, `disc_name`, `reldate`, `uname`, `reviewtext`
 (5, 'bopz vol.1', '2022-01-28', 'wazzup', 'it ok but not great -ign', 2, 2003),
 (6, 'bopz vol.4', '2022-01-29', 'hellyeah', 'ufwfuwef', 3, 2003),
 (7, 'Vol. 3: Bottom of the Barrel', '0000-00-00', 'novepro', 'Haven\'t listened to it lol', 4, 2003),
-(8, 'Vol. 3: Bottom of the Barrel', '2021-10-15', 'not_nove', 'Uhh ok', 5, 2005);
+(8, 'Vol. 3: Bottom of the Barrel', '2021-10-15', 'not_nove', 'Uhh ok', 5, 2005),
+(9, 'Vol. 3: Bottom of the Barrel', '2021-10-15', 'hater', 'this stinks', 1, 1923),
+(10, 'Vol. 3: Bottom of the Barrel', '2021-10-15', 'big_fan', 'this shi slaps ngl', 5, 2001),
+(11, 'Vol. 3: Bottom of the Barrel', '2021-10-15', 'music_critic', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 3, 1986),
+(12, 'Vol. 3: Bottom of the Barrel', '2021-10-15', 'musicman42', 'Pretty aight, me likey', 4, 2003),
+(13, 'Vol. 3: Bottom of the Barrel', '2021-10-15', 'boi', 'MASTAPIECE!', 5, 2003);
 
 --
 -- Indexes for dumped tables
@@ -69,7 +74,7 @@ ALTER TABLE `reviews`
 -- AUTO_INCREMENT for table `reviews`
 --
 ALTER TABLE `reviews`
-  MODIFY `reviewid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `reviewid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
